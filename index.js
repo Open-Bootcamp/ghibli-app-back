2315fca5635c652350c3e11b484668e6f27c5169
Andres2310 <and.23108@gmail.com>
2022-09-16 16:39:11 -0400

    add mongo conection & add cors & use routefilms

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 require('dotenv').config()
+
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+
+const mongoDBenv = process.env.DATABASE_URL
 const port = process.env.PORT
 
-const express = require('express')
-
 const app = express()
+mongoose.connect(mongoDBenv)
+
+const database = mongoose.connection
+database.on('error', (e) => {
+	console.log(e)
+})
+database.once('connected', () => {
+	console.log('Conectado a MongoDB')
+})
+
+app.use(
+	cors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		preflightContinue: false,
+		optionsSuccessStatus: 204,
+	})
+)
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+	respuesta = {
+		error: false,
+		codigo: 200,
+		mensaje: 'Servidor Activo',
+	}
+	res.send(respuesta)
+})
+
+app.use('/', require('./routes/films'))
 
 app.listen(port, () => {
 	console.log(`Servidor a la escucha en ${port}`)
