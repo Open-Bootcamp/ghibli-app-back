b3e613afafd0d90f52cde15908a31fa69f5d633d
AleVedoya <alejandravedoya@hotmail.com>
2022-09-17 11:24:27 -0300

    finished postFilm

--- controllers/controllerFilms.js
+++ controllers/controllerFilms.js
@@ -5,4 +5,16 @@ const getAllFilms = async (req, res) => {
 	res.status(200).json(films)
 }
 
-module.exports = { getAllFilms }
+const postFilm = async (req, res) => {
+	const films = new model(req.body)
+	try {
+		const guardado = await films.save()
+		res.status(200).json(guardado)
+	} catch (error) {
+		res.status(500).json({
+			error: error.mensaje,
+		})
+	}
+}
+
+module.exports = { getAllFilms, postFilm }
--- routes/films.js
+++ routes/films.js
@@ -1,21 +1,8 @@
-const { getAllFilms } = require('../controllers/controllerFilms')
+const { getAllFilms, postFilm } = require('../controllers/controllerFilms')
 const express = require('express')
 const routs = express.Router()
-const model = require('../models/films')
 
 routs.get('/films', getAllFilms)
-
-const postFilm = async (req, res) => {
-	const films = new model(req.body)
-	try {
-		const guardado = await films.save()
-		res.status(200).json(guardado)
-	} catch (error) {
-		res.status(500).json({
-			error: error.mensaje,
-		})
-	}
-}
 
 routs.post('/films', postFilm)
 
