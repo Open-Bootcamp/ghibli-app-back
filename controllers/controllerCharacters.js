e6ff11203e249866ef55ba7654f90ec43e044349
jonatanhn <jonapato00@gmail.com>
2022-09-17 13:17:53 -0500

    add routes update and delete

--- controllers/controllerCharacters.js
+++ controllers/controllerCharacters.js
@@ -1,15 +1,18 @@
 const modelCharacters = require('../models/characters')
 
+// Todos 
 const getAllCharacters = async (req, res) => {
 	const allCharacters = await modelCharacters.find()
 	res.status(200).json(allCharacters)
 }
 
+// Obtener
 const getCharacter = async (req, res) => {
 	const getDataCharacter = await modelCharacters.findById(req.params.id)
 	res.status(200).json(getDataCharacter)
 }
 
+// Enviar 
 const sendCharacters = async (req, res) => {
 	const body = req.body
 	const newCharacter = await new modelCharacters(body)
@@ -17,4 +20,28 @@ const sendCharacters = async (req, res) => {
 	res.status(200).json(data)
 }
 
-module.exports = { getAllCharacters, sendCharacters, getCharacter }
+// Actualizar 
+const updateCharacter = async (req, res) => {
+	const getDataCharacter = await modelCharacters.findByIdAndUpdate(
+		req.params.id,
+		req.body
+	)
+	res.status(200).json(getDataCharacter)
+}
+
+
+// Eliminar 
+const deleteCharacter = async (req, res) => {
+	const getDataCharacter = await modelCharacters.findByIdAndDelete(
+		req.params.id
+	)
+	res.status(200).json(getDataCharacter)
+}
+
+module.exports = {
+	getAllCharacters,
+	sendCharacters,
+	getCharacter,
+	updateCharacter,
+	deleteCharacter,
+}
